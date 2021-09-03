a7753b08950ac6242a4a51d7fb20967b01ffd2e9
Bobur-young-dev <76409850+Bobur-young-dev@users.noreply.github.com>
2021-09-04 01:25:15 +0500

    Update and rename photo_to_text.py to oyatbot.py

---
+++ oyatbot.py
@@ -0,0 +1,26 @@
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+import requests
+def start(update,context):
+    text = f'Salom {update.message.from_user.name} <b>Sura</b> va <b>Oyat</b> raqamini ketma-ketlikda jo\'nating:\n1-sura,2-oyat: <b>1,4</b>'
+    update.message.reply_html(text)
+def search(update,context):
+    if update.message.text=='/creator' or update.message.text=='/developer' or update.message.text=='/dev':
+        update.message.reply_html("<b>Developer</b>: @bobur_khamzayev")
+    else:
+        try:
+            tafsir = 'uzb-muhammadsodikmu'
+            msg = update.message.text.split(',')
+            sura = int(msg[0])
+            oyat = int(msg[1])
+            url_sura = f'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}.json'
+            url_oyat = f'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}/{oyat}.json'
+            r = requests.get(url_oyat)
+            res=r.json()['text']
+            update.message.reply_html(f'<b>{sura}-sura,{oyat}-oyat</b> :\n <i>{res}</i>')
+        except:
+            update.message.reply_text("Bunday oyat va sura topilmadi!")
+updater = Updater('1936804298:AAHU20v1aOliEweEtXqz2fl7pRJSpOeMCZU')
+updater.dispatcher.add_handler(CommandHandler('start',start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, search))
+updater.start_polling()
+updater.idle()
